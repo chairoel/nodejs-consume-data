55c543c8f7298f4bd08fa232de3f368418ae99eb
chairul <chairul.amri@ebconnect.com>
2024-09-25 14:07:28 +0700

    mapping port for order and user service

--- server.js
+++ server.js
@@ -10,4 +10,9 @@ const init = async () => {
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
+const { ORDER_SERVICE_PORT = 4000, USER_SERVICE_PORT = 5000 } = process.env;
+
+const orderService = `http://localhost:${ORDER_SERVICE_PORT}`;
+const userService = `http://localhost:${USER_SERVICE_PORT}`;
+
 init();
